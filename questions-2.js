1617a0912d7bcd6d23d1ddf5096b9d9b8eadd629
bankksar <bankk.thitiphansar@gmail.com>
2025-02-19 06:23:47 +0700

    Imported Project: HTML Library 2023

---
+++ questions-2.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "ถ้าต้องการเว้นวรรคจะต้องใช้คำสั่งอะไร?",
+    answer: "& nbsp;",
+    options: [
+      "& _;",
+      "& nbsp;",
+      "& space;",
+      "& jump;"
+    ]
+  },
+    {
+    numb: 2,
+    question: "คำสั่งใดเอาไว้ขึ้นบรรทัดใหม่",
+    answer: "BR",
+    options: [
+      "BR",
+      "HR",
+      "B",
+      "FONT"
+    ]
+  },
+    {
+    numb: 3,
+    question: "แท็ก &lt;FONT&gt; เป็นคำสั่งอะไร",
+    answer: "ถูกทุกข้อ",
+    options: [
+      "เป็นคำสั่งกำหนดขนาดฟอนต์",
+      "เป็นคำสั่งเปลี่ยนสีฟอนต์",
+      "เป็นคำสั่งจัดรูปแบบตัวอักษร",
+      "ถูกทุกข้อ"
+    ]
+  },
+    {
+    numb: 4,
+    question: "แท็กอะไรใช้ให้ตัวอักษรเป็นตัวหนา",
+    answer: "B",
+    options: [
+      "B",
+      "P",
+      "I",
+      "U"
+    ]
+  },
+    {
+    numb: 5,
+    question: "แท็กอะไรใช้ให้ตัวอักษรเป็นตัวเอียง",
+    answer: "I",
+    options: [
+      "B",
+      "P",
+      "I",
+      "U"
+    ]
+  },
+  {
+    numb: 6,
+    question: "แท็กอะไรใช้ให้ตัวอักษรมีขีดเส้นใต้",
+    answer: "U",
+    options: [
+      "B",
+      "P",
+      "I",
+      "U"
+    ]
+  },
+    {
+    numb: 7,
+    question: "คำสั่งที่ใช้กำหนดตัวอักษรเป็นหัวเรื่องมียันกี่ขนาด",
+    answer: "H1 - H7",
+    options: [
+      "H1 - H5",
+      "H1 - H4",
+      "H1 - H6",
+      "H1 - H7"
+    ]
+  },
+    {
+    numb: 8,
+    question: "&lt;P ALIGN=...&gt; ต้องใช้อะไรให้ข้อความอยู่ตรงกลาง",
+    answer: "CENTER",
+    options: [
+      "NORMAL",
+      "MEDIUM",
+      "CENTER",
+      "MIDDLE"
+    ]
+  },
+  {
+    numb: 9,
+    question: "คำสั่งใดทำให้ตัวอักษรวิ่ง",
+    answer: "MARQUEE",
+    options: [
+      "MARQUEE",
+      "RUN",
+      "PLAY",
+      "FONT"
+    ]
+  },
+    {
+    numb: 10,
+    question: "อะไร'ไม่ใช่'คุณลักษณะ(Attributes)ของคำสั่ง &lt;FONT&gt;",
+    answer: "WIDTH",
+    options: [
+      "SIZE",
+      "WIDTH",
+      "COLOR",
+      "ALIGN"
+    ]
+  },
+];
---
+++ questions-4.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "คำสั่งในทีจะใส่รูปภาพลงไป",
+    answer: "IMG SRC=",
+    options: [
+      "PHOTO SRC=",
+      "PIC SRC=",
+      "HREF=",
+      "IMG SRC="
+    ]
+  },
+    {
+    numb: 2,
+    question: "คุณลักษณะ(Attributes)ที่จะใส่กรอบให้รูปภาพ",
+    answer: "BORDER=",
+    options: [
+      "BORDER=",
+      "LINE=",
+      "STOKE=",
+      "FRAME="
+    ]
+  },
+    {
+    numb: 3,
+    question: "คุณลักษณะ(Attributes)ที่จะใส่ความกว้างให้รูปภาพ",
+    answer: "WIDTH=",
+    options: [
+      "WIDTH=",
+      "HIGHT=",
+      "ALT=",
+      "ALIGN="
+    ]
+  },
+    {
+    numb: 4,
+    question: "คุณลักษณะ(Attributes)ที่จะใส่ความสูงให้รูปภาพ",
+    answer: "HIGHT=",
+    options: [
+      "WIDTH=",
+      "HIGHT=",
+      "ALT=",
+      "ALIGN="
+    ]
+  },
+    {
+    numb: 5,
+    question: "คุณลักษณะ(Attributes)ที่จะใส่คำอธิบายรูปภาพ",
+    answer: "ALT=",
+    options: [
+      "WIDTH=",
+      "HIGHT=",
+      "ALT=",
+      "ALIGN="
+    ]
+  },
+  {
+    numb: 6,
+    question: "คุณลักษณะ(Attributes)ที่จะกำหนดตำแหน่งรูปภาพ",
+    answer: "ALIGN=",
+    options: [
+      "WIDTH=",
+      "HIGHT=",
+      "ALT=",
+      "ALIGN="
+    ]
+  },
+    {
+    numb: 7,
+    question: "คุณลักษณะ(Attributes)ที่จะกำหนดระยะห่างจากรูปภาพในแนวนอน",
+    answer: "HSPACE=",
+    options: [
+      "BORDER=",
+      "HSPACE=",
+      "MARGIN=",
+      "VSPACE="
+    ]
+  },
+    {
+    numb: 8,
+    question: "คุณลักษณะ(Attributes)ที่จะกำหนดระยะห่างจากรูปภาพในแนวตั้ง",
+    answer: "VSPACE=",
+    options: [
+      "BORDER=",
+      "HSPACE=",
+      "MARGIN=",
+      "VSPACE="
+    ]
+  },
+  {
+    numb: 9,
+    question: "คำสั่งใดเป็นคำใส่ในการแทรกเพลงบนเว็บไซต์",
+    answer: "BGSOUND SRC=",
+    options: [
+      "HREF=",
+      "IMG SRC=",
+      "BGSOUND SRC=",
+      "LINK="
+    ]
+  },
+    {
+    numb: 10,
+    question: "คุณลักษณะ(Attributes)ที่จะกำหนดให้เพลงเล่นวนซ้ำๆ",
+    answer: "LOOP='Infinite'",
+    options: [
+      "LOOP='Infinite'",
+      "Infinite",
+      "LOOP",
+      "ไม่มีข้อถูก"
+    ]
+  },
+];
