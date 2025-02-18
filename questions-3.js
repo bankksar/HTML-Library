0cc91460c33451b66b90d782f3adbc762021ffe5
bankksar <bankk.thitiphansar@gmail.com>
2025-02-19 06:50:31 +0700

    Imported Project: HTML Library 2023

---
+++ questions-3.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "การสร้าง List แบ่งเป็นกี่ประเภท?",
+    answer: "7 ประเภท",
+    options: [
+      "4 ประเภท",
+      "5 ประเภท",
+      "6 ประเภท",
+      "7 ประเภท"
+    ]
+  },
+    {
+    numb: 2,
+    question: "การกำหนดหัวข้อย่อยแบบไม่ได้เรียงลำดับหรือแบบปกติต้องใช้คำสั่งอะไร",
+    answer: "UL (Unordered List)",
+    options: [
+      "DL (Definition List)",
+      "UL (Unordered List)",
+      "LI (List Item)",
+      "OL (Order List)"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Unordered List เป็นประเภท Disc คืออะไร",
+    answer: "วงกลมทึบ",
+    options: [
+      "วงกลมทึบ",
+      "วงกลมโปร่งใส",
+      "สี่เหลี่ยมทึบ",
+      "สามเหลี่ยม"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Unordered List เป็นประเภท Square คืออะไร",
+    answer: "สี่เหลี่ยมทึบ",
+    options: [
+      "วงกลมทึบ",
+      "วงกลมโปร่งใส",
+      "สี่เหลี่ยมทึบ",
+      "สามเหลี่ยม"
+    ]
+  },
+    {
+    numb: 5,
+    question: "การจัดรายการให้มีการเรียงลำดับ โดยตัวเลขหรือตัวอักษรนำหน้ารายการ และจะเรียงลำดับจากน้อยไปหามาก",
+    answer: "OL (Order List)",
+    options: [
+      "DL (Definition List)",
+      "UL (Unordered List)",
+      "LI (List Item)",
+      "OL (Order List)"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Order List ถ้าต้องการเรียงลำดับโดยใช้ตัวอักษรโรมันพิมพ์เล็ก",
+    answer: "i",
+    options: [
+      "1",
+      "i",
+      "I",
+      "a"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Order List ถ้าต้องการเรียงลำดับโดยใช้ตัวอักษรอักฤษพิมพ์เล็ก",
+    answer: "a",
+    options: [
+      "1",
+      "i",
+      "I",
+      "a"
+    ]
+  },
+    {
+    numb: 8,
+    question: "การจัดรายการที่ใช้สำหรับต้องการแสดงข้อความและคำอธิบายสลับกันไป",
+    answer: "DL (Definition List)",
+    options: [
+      "DL (Definition List)",
+      "UL (Unordered List)",
+      "LI (List Item)",
+      "OL (Order List)"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Definition List คำสั่งใดคือการคำจำกัดความ",
+    answer: "DT",
+    options: [
+      "DT",
+      "DL",
+      "DD",
+      "DB"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Definition List คำสั่งใดคือการความหมายหรือคำอธิบาย",
+    answer: "DD",
+    options: [
+      "DT",
+      "DL",
+      "DD",
+      "DB"
+    ]
+  },
+];
---
+++ questions-8.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "CSS ย่อมาจากอะไร?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Cascading Status Sheets",
+      "Cascading Stoke Sheets",
+      "Cascading Style Sheets",
+      "Cascading Store Shhets"
+    ]
+  },
+    {
+    numb: 2,
+    question: "CSS มีกี่รูปแบบ",
+    answer: "3 รูปแบบ",
+    options: [
+      "1 รูปแบบ",
+      "2 รูปแบบ",
+      "3 รูปแบบ",
+      "4 รูปแบบ"
+    ]
+  },
+    {
+    numb: 3,
+    question: "CSS มี 3 รูปแบบมีอะไรบ้าง",
+    answer: "Inline ,Embed ,External",
+    options: [
+      "Inline ,Embed ,External",
+      "Internal ,Centernal ,External",
+      "Inline ,Centernal ,External",
+      "ไม่มีข้อถูก"
+    ]
+  },
+    {
+    numb: 4,
+    question: "การใช้ CSS รูปแบบใดแทรกในคำสั่ง",
+    answer: "Inline",
+    options: [
+      "Inline",
+      "Embed",
+      "Externl",
+      "ไม่มีรูปแบบดังกล่าว"
+    ]
+  },
+    {
+    numb: 5,
+    question: "การใช้ CSS รูปแบบใดสร้าง Document",
+    answer: "External",
+    options: [
+      "Inline",
+      "Embed",
+      "Externl",
+      "ไม่มีรูปแบบดังกล่าว"
+    ]
+  },
+  {
+    numb: 6,
+    question: "การใช้ CSS รูปแบบใดแทรกในHead",
+    answer: "Embed",
+    options: [
+      "Inline",
+      "Embed",
+      "Externl",
+      "ไม่มีรูปแบบดังกล่าว"
+    ]
+  },
+    {
+    numb: 7,
+    question: "นามสกุลไฟล์ Document ของ CSS คืออะไร",
+    answer: "css",
+    options: [
+      "php",
+      "txt",
+      "html",
+      "css"
+    ]
+  },
+    {
+    numb: 8,
+    question: "CSS มีหน้าที่อะไร",
+    answer: "ตบแต่งเว็บไซต์",
+    options: [
+      "ทำให้ไฟล์ใช้งานได้สะดวก",
+      "ตบแต่งเว็บไซต์",
+      "ทำให้ไฟล์มีความรวดเร็ว",
+      "จัดระเบียบโค้ด"
+    ]
+  },
+  {
+    numb: 9,
+    question: "ถ้าทำเว็บไซต์แล้วมีแค่ html อย่างเดียว",
+    answer: "ทุกข้อที่กล่าว",
+    options: [
+      "เว็บไซต์ไม่เป็นระเบียบ",
+      "เว็บไซต์ไม่มีสีสันสวยงาม",
+      "เว็บไซต์ดูเรียบง่ายเกินไป",
+      "ทุกข้อที่กล่าว"
+    ]
+  },
+    {
+    numb: 10,
+    question: "การกำหนดคลาสที่จะทำการตบแต่งต้องใช้อะไร",
+    answer: "ถูกทั้งสองข้อ",
+    options: [
+      "ID",
+      "CLASS",
+      "ถูกทั้งสองข้อ",
+      "ไม่มีข้อถูก"
+    ]
+  },
+];
